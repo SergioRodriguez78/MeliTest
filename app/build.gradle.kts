2c3e6d75178629dc6e375fc3f1b8ddfd45f038b9
SergioR <sergioandresrodriguez78@gmail.com>
2025-01-10 15:20:26 -0500

    Add koin and retrofit - add koin in project - add retrofit provider - add kotlin serialization - organize libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,12 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.mercadolibre.melitest"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mercadolibre.melitest"
@@ -41,14 +42,30 @@ android {
 
 dependencies {
 
+    /*** Core ***/
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    /*** Compose ***/
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    /*** lifecycle ***/
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    /*** DI ***/
+    implementation(libs.koin)
+
+    /*** Serialization ***/
+    implementation(libs.kotlinx.serialization)
+
+    /*** Network ***/
+    implementation(libs.bundles.network)
+
+    /*** Testing ***/
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
